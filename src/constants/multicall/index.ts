4044ce3bb9cf6e36af02c3756b43b72c47e74603
liubaoli <uoi00@qq.com>
2021-06-26 17:12:26 +0800

    init

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,12 @@
+import { ChainId } from 'wpf-ren-swap-sdk'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
+  [ChainId.KOVAN]: '0x371D423c1295faCA81C9BeBB4B9FB42dE7079194',
+  [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
+  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e'
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
